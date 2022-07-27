8688162b9328078f3000ec12429f6e90c2c1a8c2
xodus05 <82927278+xodus05@users.noreply.github.com>
2022-07-27 14:20:25 +0900

    초기 커밋

---
+++ app/src/main/java/kr/hs/emirims2121/mirim_project_0727_/SecondActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirims2121.mirim_project_0727_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
