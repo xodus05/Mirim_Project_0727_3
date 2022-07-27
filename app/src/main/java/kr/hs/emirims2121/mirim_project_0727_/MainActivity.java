774affeb4e56b0d784451dc7884d97a64e825482
xodus05 <82927278+xodus05@users.noreply.github.com>
2022-07-27 15:37:39 +0900

    명시적 Intent 데이터 전달 완성

--- app/src/main/java/kr/hs/emirims2121/mirim_project_0727_/MainActivity.java
+++ app/src/main/java/kr/hs/emirims2121/mirim_project_0727_/MainActivity.java
@@ -2,13 +2,43 @@ package kr.hs.emirims2121.mirim_project_0727_;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+    RadioGroup rg;
+    EditText editTitle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rg = findViewById(R.id.rg);
+        editTitle = findViewById(R.id.edit_title);
+        Button btnStart = findViewById(R.id.btn_start);
+        btnStart.setOnClickListener(btnStartListener);
     }
+
+    View.OnClickListener btnStartListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Class c = null;
+            switch (rg.getCheckedRadioButtonId()){
+                case R.id.rb_second:
+                    c = SecondActivity.class;
+                    break;
+                case R.id.rb_third:
+                    c = ThirdActivity.class;
+                    break;
+            }
+            Intent intent = new Intent(getApplicationContext(), c);
+            intent.putExtra("name", editTitle.getText().toString());
+            startActivity(intent);
+        }
+    };
 }
